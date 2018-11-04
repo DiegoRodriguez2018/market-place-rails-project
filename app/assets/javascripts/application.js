f5747806a06b51cb6289fc2c24605b997fc962ab
Eathan <eathanlangford@gmail.com>
2018-11-05 03:32:29 +1100

    product styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,11 @@
 //= require bootstrap
 
 //= require_tree .
+$("nav ul li").click(function(){
+    var xcoord = $(this).data("xcoord");
+    
+    $("nav div").stop().animate({marginLeft:xcoord}, 500, "easeInOutExpo");
+    $(this).addClass("active");
+    $("nav ul li").not(this).removeClass("active");
+    
+  });
